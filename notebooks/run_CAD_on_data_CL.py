701c7d268874e2d9331510e235de2c49af31609e
Jayse Weaver <jayse.weaver@fda.hhs.gov>
2025-05-20 11:59:40 -0400

    daily commit notebook updates

--- notebooks/run_CAD_on_data_CL.py
+++ notebooks/run_CAD_on_data_CL.py
@@ -6,6 +6,7 @@ import numpy as np
 from sklearn import metrics
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+import re
 
 from image_utils import *
 from model_utils import *
@@ -25,11 +26,12 @@ print('using device ' + str(device))
 
 #dataset_path = Path('../datasets')
 dataset_path = Path('/projects01/didsr-aiml/jayse.weaver/insilicoich/')
-#dataset_names = ['mixed_2500_count']
-dataset_names = ['varymA_70_count500', 'varymA_280_count500', 'varymA_600_count500']
+dataset_names = ['all_ages_3000']
+#dataset_names = ['varymA_70_count500', 'varymA_280_count500', 'varymA_600_count500']
 
 prepath = '/home/jayse.weaver/model_files/'
-model_names = ['CAD_1', 'CAD_2', 'CAD_3']
+#model_names = ['CAD_1', 'CAD_2', 'CAD_3']
+model_names = ['CAD_3']
 
 save_dir = '/home/jayse.weaver/temp_images/new/' # optional save directory if save_jpg or save_dcm = True
 
@@ -49,6 +51,8 @@ for model in model_names:
         cases = sorted(os.listdir(path))
         cases = [case for case in cases if case.startswith('case')]
 
+        #cases = ['case_000', 'case_001', 'case_003', 'case_004', 'case_005', 'case_007']
+
         labels_syn = []
         pred_syn = []
         type_syn = []
@@ -66,26 +70,37 @@ for model in model_names:
         with tqdm(total=max_count) as pbar:
             for index, case in enumerate(cases):
 
-                print('processing : ' + str(case))
+                #print('processing : ' + str(case))
 
                 pbar.update(1)
 
                 id = str(case) + '_std'
                 if os.path.isdir(Path.joinpath(path, case, 'lesion_masks/')): # check if case has mask and therefore hemorrhage
-                    labels_syn.append(1)
+                    try:
+                        # if case has hemorrhage, extract metadata from dataframe
+                        # TODO: fix insilicoICH metadata generation (currently messy with added strings and brackets)
+                        temp_df = metadata_dropna.loc[metadata_dropna['Name'] == case]
+                        intensity = temp_df['LesionAttenuation(HU)'].unique()[0]
+                        lesion_type = temp_df['Subtype'].unique()[0] #.split("'")[1]
+                        #volume_syn.append(temp_df['LesionVolume(mL)'].astype(float).sum())
+                        volume = temp_df['LesionVolume(mL)'].apply(lambda x: x.replace('[','').replace(']','')).astype(float).sum()
 
-                    # if case has hemorrhage, extract metadata from dataframe
-                    # TODO: fix insilicoICH metadata generation (currently messy with added strings and brackets)
-                    temp_df = metadata_dropna.loc[metadata_dropna['Name'] == case]
-
-                    lesion_type = temp_df['Subtype'].unique()[0] #.split("'")[1]
-                    type_syn.append(lesion_type)
-
-                    volume_syn.append(temp_df['LesionVolume(mL)'].sum())
-
-                    intensity = float(temp_df['LesionAttenuation(HU)'].unique()[0][1:-1])
-                    intensity_syn.append(intensity)
-
+                        #intensity_string = temp_df['LesionAttenuation(HU)'].unique()[0][1:-1]
+                        # remove np.float64... may not be necessary depending on age of dataset...
+                        # above line will be sufficient for older datasets; check 'LesionAttenuation' cells before running
+                        #intensity = re.findall(r"\(([^)]+)\)", intensity_string)[0] # regex wizardry
+                        
+                        # try extracting all metadata before appending
+                        volume_syn.append(volume)
+                        intensity_syn.append(intensity)
+                        type_syn.append(lesion_type)
+                        labels_syn.append(1)
+                    except:
+                        print('Error')
+                        labels_syn.append(0)
+                        type_syn.append('None')
+                        volume_syn.append('NaN')
+                        intensity_syn.append('NaN')
                 else: # case has no mask, therefore no hemorrhage
                     labels_syn.append(0)
                     type_syn.append('None')
