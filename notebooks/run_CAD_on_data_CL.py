cb651181486b7ca4b51d3ef9667bbe099f134d23
Jayse Weaver <jayse.weaver@fda.hhs.gov>
2025-05-22 14:44:28 -0400

    update CAD notebooks with consistent headers and parsing

--- notebooks/run_CAD_on_data_CL.py
+++ notebooks/run_CAD_on_data_CL.py
@@ -26,12 +26,11 @@ print('using device ' + str(device))
 
 #dataset_path = Path('../datasets')
 dataset_path = Path('/projects01/didsr-aiml/jayse.weaver/insilicoich/')
-dataset_names = ['all_ages_3000']
+dataset_names = ['test_3']
 #dataset_names = ['varymA_70_count500', 'varymA_280_count500', 'varymA_600_count500']
 
 prepath = '/home/jayse.weaver/model_files/'
-#model_names = ['CAD_1', 'CAD_2', 'CAD_3']
-model_names = ['CAD_3']
+model_names = ['CAD_1', 'CAD_2', 'CAD_3']
 
 save_dir = '/home/jayse.weaver/temp_images/new/' # optional save directory if save_jpg or save_dcm = True
 
@@ -51,13 +50,12 @@ for model in model_names:
         cases = sorted(os.listdir(path))
         cases = [case for case in cases if case.startswith('case')]
 
-        #cases = ['case_000', 'case_001', 'case_003', 'case_004', 'case_005', 'case_007']
-
-        labels_syn = []
-        pred_syn = []
-        type_syn = []
-        volume_syn = []
-        intensity_syn = []
+        # initialize empty lists for results CSV
+        attenuation_list = []
+        volume_list = []
+        type_list = []
+        labels_list = []
+        preds_list = []
 
         if options['native_resolution']:
             print('Processing in native resolution')
@@ -70,53 +68,47 @@ for model in model_names:
         with tqdm(total=max_count) as pbar:
             for index, case in enumerate(cases):
 
-                #print('processing : ' + str(case))
-
                 pbar.update(1)
 
-                id = str(case) + '_std'
+                id = str(case)
                 if os.path.isdir(Path.joinpath(path, case, 'lesion_masks/')): # check if case has mask and therefore hemorrhage
                     try:
                         # if case has hemorrhage, extract metadata from dataframe
                         # TODO: fix insilicoICH metadata generation (currently messy with added strings and brackets)
                         temp_df = metadata_dropna.loc[metadata_dropna['Name'] == case]
-                        intensity = temp_df['LesionAttenuation(HU)'].unique()[0]
-                        lesion_type = temp_df['Subtype'].unique()[0] #.split("'")[1]
-                        #volume_syn.append(temp_df['LesionVolume(mL)'].astype(float).sum())
-                        volume = temp_df['LesionVolume(mL)'].apply(lambda x: x.replace('[','').replace(']','')).astype(float).sum()
-
-                        #intensity_string = temp_df['LesionAttenuation(HU)'].unique()[0][1:-1]
-                        # remove np.float64... may not be necessary depending on age of dataset...
-                        # above line will be sufficient for older datasets; check 'LesionAttenuation' cells before running
-                        #intensity = re.findall(r"\(([^)]+)\)", intensity_string)[0] # regex wizardry
                         
+                        attenuation = float(temp_df['LesionAttenuation(HU)'].unique()[0].replace('[','').replace(']',''))
+                        volume = temp_df['LesionVolume(mL)'].apply(lambda x: x.replace('[','').replace(']','')).astype(float).sum()
+                        lesion_type = temp_df['Subtype'].unique()[0].replace('[','').replace(']','')
+
                         # try extracting all metadata before appending
-                        volume_syn.append(volume)
-                        intensity_syn.append(intensity)
-                        type_syn.append(lesion_type)
-                        labels_syn.append(1)
+                        attenuation_list.append(attenuation)
+                        volume_list.append(volume)
+                        type_list.append(lesion_type)
+                        labels_list.append(1)
                     except:
                         print('Error')
-                        labels_syn.append(0)
-                        type_syn.append('None')
-                        volume_syn.append('NaN')
-                        intensity_syn.append('NaN')
+                        attenuation_list.append('NaN')
+                        volume_list.append('NaN')
+                        type_list.append('None')
+                        labels_list.append(0)
+                       
                 else: # case has no mask, therefore no hemorrhage
-                    labels_syn.append(0)
-                    type_syn.append('None')
-                    volume_syn.append('NaN')
-                    intensity_syn.append('NaN')
+                    attenuation_list.append('NaN')
+                    volume_list.append('NaN')
+                    type_list.append('None')
+                    labels_list.append(0)
 
                 dcm_path = Path.joinpath(path, case, 'dicoms/')
 
                 img, files = prepare_images(dcm_path, options, id, save_dir)
 
                 output = classify_images(img, options, model_path, device)
-                pred_syn.append(np.max(output[:, -1]))
+                preds_list.append(np.max(output[:, -1]))
 
                 if options['verbose']: print(np.max(output[:, -1]))
 
-        fpr_syn, tpr_syn, thresholds_syn = metrics.roc_curve(labels_syn, pred_syn, pos_label=1)
+        fpr_syn, tpr_syn, thresholds_syn = metrics.roc_curve(labels_list, preds_list, pos_label=1)
         roc_df_syn = pd.DataFrame(zip(fpr_syn, tpr_syn, thresholds_syn),columns = ["FPR","TPR","Threshold"])
 
         roc_auc_syn = metrics.auc(fpr_syn, tpr_syn)
@@ -124,12 +116,12 @@ for model in model_names:
         all_cases = cases.copy()
 
         dfsyn = pd.DataFrame()
-        dfsyn['case'] = all_cases
-        dfsyn['truth'] = labels_syn
-        dfsyn['pred'] = pred_syn
-        dfsyn['type'] = type_syn
-        dfsyn['volume'] = volume_syn
-        dfsyn['intensity'] = intensity_syn
+        dfsyn['Name'] = all_cases
+        dfsyn['TruthLabel'] = labels_list
+        dfsyn['ModelPrediction'] = preds_list
+        dfsyn['LesionAttenuation(HU)'] = attenuation_list
+        dfsyn['LesionVolume(mL)'] = volume_list
+        dfsyn['Subtype'] = type_list
 
         if options['save_csv']: dfsyn.to_csv(path / (str(dataset) + '_' + model + '_results.csv'))
         
