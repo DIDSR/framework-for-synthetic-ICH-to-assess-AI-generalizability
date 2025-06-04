9a12a3660ed2577d8344c6bacb8f009d1e123bab
Jayse Weaver <jayse.weaver@fda.hhs.gov>
2025-06-04 11:48:14 -0400

    add study locations to bash script

--- notebooks/figure_run_all.sh
+++ notebooks/figure_run_all.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
+echo "Setting environment variables..."
+export BASE_DIR=/projects01/didsr-aiml/jayse.weaver/insilicoich/
+export STUDY_NAME=manuscript_100_280mA_wME
+export EXPERIMENT_NAME=kVp_mA_variation
+echo "Running all notebooks..."
 jupyter nbconvert --to notebook --execute fig3-5_view_six_examples.ipynb
 jupyter nbconvert --to notebook --execute fig4_compare_synth_real_distributions.ipynb
 jupyter nbconvert --to notebook --execute fig6_kV_mA_variation.ipynb
 jupyter nbconvert --to notebook --execute fig7_create_multimodel_ROC.ipynb
 jupyter nbconvert --to notebook --execute fig8_compare_false_negatives.ipynb
 rm -rf *.nbconvert.*
+echo "Done."
