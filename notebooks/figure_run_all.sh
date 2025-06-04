946ff69022d5048b8e58b5d304e04ad82c8689a8
Jayse Weaver <jayse.weaver@fda.hhs.gov>
2025-06-04 11:52:52 -0400

    added comment for context

--- notebooks/figure_run_all.sh
+++ notebooks/figure_run_all.sh
@@ -6,7 +6,7 @@ export EXPERIMENT_NAME=kVp_mA_variation
 echo "Running all notebooks..."
 jupyter nbconvert --to notebook --execute fig3-5_view_six_examples.ipynb
 jupyter nbconvert --to notebook --execute fig4_compare_synth_real_distributions.ipynb
-jupyter nbconvert --to notebook --execute fig6_kV_mA_variation.ipynb
+jupyter nbconvert --to notebook --execute fig6_kV_mA_variation.ipynb # uses EXPERIMENT_NAME for kVp/mA variation
 jupyter nbconvert --to notebook --execute fig7_create_multimodel_ROC.ipynb
 jupyter nbconvert --to notebook --execute fig8_compare_false_negatives.ipynb
 rm -rf *.nbconvert.*
