92990674adf9260dd24e97ff7dee28dc76076db5
brandon.nelson <brandon.nelson@scl.fda.gov>
2025-06-06 13:39:30 -0400

    added option to input BASE_DIR on first runall

--- run_all.sh
+++ run_all.sh
@@ -1,12 +1,35 @@
 #!/usr/bin/env bash
 echo "Setting environment variables..."
-export STUDY_NAME=manuscript_100_280mA_wME
-export EXPERIMENT_NAME=kVp_mA_variation
+
+# Define the default path
+source .env
+if [ $? -ne 0 ]; then
+  echo "Error: .env file not found or could not be sourced."
+  exit 1
+fi
+
+DEFAULT_PATH="./datasets"
+
+# Check if the BASE_DIR environment variable is not set
+if [ -z "${BASE_DIR}" ]; then
+  echo "The BASE_DIR environment variable is not set."
+  read -p "Please enter the path for BASE_DIR [default: ${DEFAULT_PATH}]: " user_input
+
+  # If the user input is empty, use the default path
+  if [ -z "${user_input}" ]; then
+    export BASE_DIR="${DEFAULT_PATH}"
+    echo "No input provided. Using default path."
+  else
+    # Otherwise, use the path provided by the user
+    export BASE_DIR="${user_input}"
+  fi
+fi
+
 echo "Running all notebooks..."
 jupyter nbconvert --to notebook --execute --inplace notebooks/fig3-5_view_six_examples.ipynb
 jupyter nbconvert --to notebook --execute --inplace notebooks/fig4_compare_synth_real_distributions.ipynb
 jupyter nbconvert --to notebook --execute --inplace notebooks/fig6_kV_mA_variation.ipynb # uses EXPERIMENT_NAME for kVp/mA variation
 jupyter nbconvert --to notebook --execute --inplace notebooks/fig7_create_multimodel_ROC.ipynb
 jupyter nbconvert --to notebook --execute --inplace notebooks/fig8_compare_false_negatives.ipynb
-# rm -rf notebooks/*.nbconvert.*
+
 echo "Done."
